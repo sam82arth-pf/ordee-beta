eeafcc87f3769229a8a82b3d6201084c46ecd578
inzamam1 <inzamamsayyed10@gmail.com>
2021-01-01 23:52:38 +0530

    Users being redirected when logged in

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { db, auth } from "./firebase";
 import "./LoginForm.css";
-import { Link,Redirect  } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import Cards from "./Cards";
 import Navbar from "./Navbar";
 
-
 function LoginForm() {
-    const [username, setUsername] = useState("");
+  const [username, setUsername] = useState("");
+  const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
   const container = document.getElementById("container");
-  
-
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
@@ -25,7 +23,6 @@ function LoginForm() {
     });
     return () => {
       unsubscribe();
-      
     };
   }, [user]);
 
@@ -34,19 +31,19 @@ function LoginForm() {
     auth
       .signInWithEmailAndPassword(email, password)
       .catch((error) => alert(error.message));
-   
   };
   const SignUp = (event) => {
     event.preventDefault();
     auth
       .createUserWithEmailAndPassword(email, password)
       .then((cred) => {
-        return db.collection("posts").doc(cred.user.uid).set({
+        return db.collection("Restaurant").doc(cred.user.uid).set({
           Name: username,
+          Phone: phone,
+          Email: email,
         });
       })
       .catch((error) => alert(error.message));
-   
   };
   const signUpButton = () => {
     container.classList.add("right-panel-active");
@@ -57,83 +54,92 @@ function LoginForm() {
   };
 
   return (
-
-
     <div className="container" id="container">
-{user?(user.email === "k@g.com" ? (
+      {user ? (
+        user.email === "a@g.com" ? (
           <Redirect to="/Admin" />
         ) : (
-          <Redirect to="/Test"/>
-        )):(console.log('hello'))}
-      <div className="form-container sign-up-container">
-        <form action="#">
-          <h1>Create Account</h1>
-          
-          <div className="social-container">
-            <a href="#" className="social">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="#" className="social">
-              <i className="fab fa-google-plus-g"></i>
-            </a>
-            <a href="#" className="social">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
+          <div>
+            <Navbar />
+            <Cards user={user} />
           </div>
-          <span>or use your email for registration</span>
-          <input type="text" placeholder="Name"  value={username}
-                onChange={(e) => setUsername(e.target.value)}/>
-          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-          <input type="password" placeholder="Password" value={password}
-                onChange={(e) => setPassword(e.target.value)}/>
-          <button onClick={SignUp}>Sign Up</button>
-        </form>
-        
-      </div>
-      <div className="form-container sign-in-container">
-        <form action="#">
-          <h1>Sign in</h1>
-          <div className="social-container">
-            <a href="#" className="social">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="#" className="social">
-              <i className="fab fa-google-plus-g"></i>
-            </a>
-            <a href="#" className="social">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
+        )
+      ) : (
+        <div>
+          <div className="form-container sign-up-container">
+            <form action="#">
+              <h1>Create Account</h1>
+
+              <input
+                type="text"
+                placeholder="Name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Contact Number"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button onClick={SignUp}>Sign Up</button>
+            </form>
           </div>
-          <span>or use your account</span>
-          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-          <input type="password" placeholder="Password" value={password}
-                onChange={(e) => setPassword(e.target.value)}/>
-          <a href="#">Forgot your password?</a>
-          <button onClick={Login}>Sign in</button>
-        </form>
-      </div>
-      <div className="overlay-container">
-        <div className="overlay">
-          <div className="overlay-panel overlay-left">
-            <h1>Welcome Back!</h1>
-            <p>
-              To keep connected with us please login with your personal info
-            </p>
-            <button className="ghost"  id="signIn" onClick={signInButton}>
-                {/*check for id here as refreshing the page does not support animation and displays error */}
-              Sign In
-            </button>
+          <div className="form-container sign-in-container">
+            <form action="#">
+              <h1>Sign in</h1>
+
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <a>Forgot your password?</a>
+              <button onClick={Login}>Sign in</button>
+            </form>
           </div>
-          <div className="overlay-panel overlay-right">
-            <h1>Hello,</h1>
-            <p>Enter your personal details and get on board with us</p>
-            <button className="ghost" id="containe" onClick={signUpButton}>
-              Sign up
-            </button>
+          <div className="overlay-container">
+            <div className="overlay">
+              <div className="overlay-panel overlay-left">
+                <h1>Welcome Back!</h1>
+                <p>
+                  To keep connected with us please login with your personal info
+                </p>
+                <button className="ghost" id="signIn" onClick={signInButton}>
+                  {/*check for id here as refreshing the page does not support animation and displays error */}
+                  Sign In
+                </button>
+              </div>
+              <div className="overlay-panel overlay-right">
+                <h1>Hello,</h1>
+                <p>Enter your personal details and get on board with us</p>
+                <button className="ghost" id="contaier" onClick={signUpButton}>
+                  Sign up
+                </button>
+              </div>
+            </div>
           </div>
-          
         </div>
-      </div>
+      )}
     </div>
   );
 }
