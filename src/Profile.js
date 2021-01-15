2563d9f614691cf872aadefa5b4b2b98d43c6900
Daksha Hedge <dakshahegde5@gmail.com>
2021-01-16 00:04:16 +0530

    profile page

--- src/Profile.js
+++ src/Profile.js
@@ -1,8 +1,11 @@
-import React from 'react'
 import Navbar from './Navbar.js';
+import React, { useState, useEffect } from "react";
+import { db, auth } from "./firebase";
+import { Link, Redirect,Route } from "react-router-dom";
+import './Profile.css';
 
-function Profile({username}) {
-const [username, setUsername] = useState("");
+function Profile({user1}){
+  const [username, setUsername] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [address1,setAddress1]=useState("");
@@ -10,23 +13,18 @@ const [username, setUsername] = useState("");
   const [state,setState]=useState("");
   const [country,setCountry]=useState("");
   const [postalcode,setPostalCode]=useState("");
+  const [password, setPassword] = useState("");
     return (
         <div className="profile">
-            <Navbar username={username} />
-            <form action="#">
-              <h1>Create Account</h1>
-
-              <input
-                type="text"
+            <Navbar username={user1} />
+            <div className="form">
+            
+            <div className="name_email">
+            <input
+                type="username"
                 placeholder="Name"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Contact Number"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
               />
               <input
                 type="email"
@@ -34,15 +32,57 @@ const [username, setUsername] = useState("");
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
+              </div>
+              <div className="contact_postal">
               <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                type="phone"
+                placeholder="Contact Number"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
               />
-              <button onClick={SignUp}>Sign Up</button>
-            </form>
-        </div>
+              <input
+                type="postalcode"
+                placeholder="Postal Code"
+                value={postalcode}
+                onChange={(e) => setPostalCode(e.target.value)}
+              />
+             
+              </div>
+              <div className="address">
+              <input
+                type="address1"
+                placeholder="Address Line 1"
+                value={address1}
+                onChange={(e) => setAddress1(e.target.value)}
+              />
+               <input
+                type="address2"
+                placeholder="Address Line 2"
+                value={address2}
+                onChange={(e) => setAddress2(e.target.value)}
+              />
+              </div>
+              <div className="state_country">
+              <input
+                type="country"
+                placeholder="Country"
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+              />
+              <input
+                type="state"
+                placeholder="State"
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+              />
+              </div>
+              <div className="profile_button">
+              <button>Update</button>
+              </div>
+             </div>
+             </div>
+             
+        
     )
 }
 
