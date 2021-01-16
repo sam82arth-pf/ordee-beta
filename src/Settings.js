94db1269784e20cc125c2553102a43611b9396c1
kshitijijari <ijarikshitij@gmail.com>
2021-01-16 19:33:39 +0530

    Add button Added

--- src/Settings.js
+++ src/Settings.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { db } from "./firebase";
+import { Button } from "@material-ui/core";
 
 
 function Settings({username}) {
+
+  const Add_Card=(event)=>{
+    event.preventDefault();
+    return db.collection('Restaurant').doc(username.uid).collection("tables").add({
+      table:'Hey',
+    })
+  }
     return (
         <div>
         <Navbar username={username} />
-          <h1>hello</h1>
+        <div>
+        <h1>Click to Add tables </h1>
+           <Button onClick={Add_Card}>Add</Button>
+        </div> 
+         
         </div>
     )
 }
