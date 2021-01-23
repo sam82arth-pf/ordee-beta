ccadaf4be7a34f839e2c53bbd9f34dd728c96988
kshitijijari <ijarikshitij@gmail.com>
2021-01-23 15:23:11 +0530

    added table id to database

--- src/Settings.js
+++ src/Settings.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Navbar from './Navbar'
 import { db } from "./firebase";
 import firebase from 'firebase';
@@ -7,13 +7,17 @@ import { Button } from "@material-ui/core";
 
 function Settings({username}) {
 
+  const [id,setId] = useState(0);
+  
   const Add_Card=(event)=>{
+   setId(id + 1)
     event.preventDefault();
     return db.collection('Restaurant').doc(username.uid).collection("tables").add({
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      table:'Hey'
+      table:'Hey',
+      id:id
     })
-  }
+    }
     return (
         <div>
         <Navbar username={username} />
